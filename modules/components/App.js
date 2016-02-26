b1833fd605672cb2b39119a20f8476e41da8c3f9
Evan Corley <eccorley@gmail.com>
2016-02-25 23:44:10 -0800

    remove reptiles

--- modules/components/App.js
+++ modules/components/App.js
@@ -10,12 +10,9 @@ export default React.createClass({
         <h1>Welcome to your app.</h1>
         <ul>
           <li><IndexLink to="/">Home</IndexLink></li>
-          <li><Link to="/dragon">A DRAGON!</Link></li>
-          <li><Link to="/not-dragon">An old URL to a DRAGON!</Link></li>
         </ul>
         {this.props.children}
       </div>
     )
   }
 })
-
